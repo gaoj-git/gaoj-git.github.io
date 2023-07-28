fa25e2bf04540688e9c1eb2675481584804a5884
gj <971004367@qq.com>
2023-07-28 11:29:47 +0800

    功能开发

--- static/js/blog.js
+++ static/js/blog.js
@@ -12,6 +12,7 @@
   console.info('%c Author %c' + author, style1, style2)
   console.info('%c Build  %c' + build, style1, style2)
   console.info('%c GitHub %c' + github, style1, style2)
+  setInterval(setTime, 1000);
 })()
 
 /**
@@ -471,3 +472,39 @@ blog.addLoadEvent(function () {
     })
   }
 })
+
+
+function secondToDate(second) {
+    if (!second) {
+      return 0;
+    }
+    var time = new Array(0, 0, 0, 0, 0);
+    if (second >= 365 * 24 * 3600) {//计算年
+      time[0] = parseInt(second / (365 * 24 * 3600));
+      second %= 365 * 24 * 3600;
+    }
+    if (second >= 24 * 3600) {//计算天
+      time[1] = parseInt(second / (24 * 3600));
+      second %= 24 * 3600;
+    }
+    if (second >= 3600) {//计算时
+      time[2] = parseInt(second / 3600);
+      second %= 3600;
+    }
+    if (second >= 60) {//计算分
+      time[3] = parseInt(second / 60);
+      second %= 60;
+    }
+    if (second > 0) {//计算秒
+      time[4] = second;
+    }
+    return time;
+  }
+  function setTime() {
+    var create_time = Math.round(new Date(Date.UTC(2022, 5, 1, 0, 0, 0)).getTime() / 1000);//设置起始时间为2017年1月1日0点整，注意月份取值是0-11。
+    var timestamp = Math.round((new Date().getTime() + 8 * 60 * 60 * 1000) / 1000);
+    currentTime = secondToDate((timestamp - create_time));
+    currentTimeHtml = currentTime[0] + '年' + currentTime[1] + '天' + currentTime[2] + '时' + currentTime[3] + '分' + currentTime[4] + '秒';
+    document.getElementById("show_time").innerHTML = currentTimeHtml;
+  }
+
